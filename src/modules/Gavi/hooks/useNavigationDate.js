abaa5335ea4c58d1e287d5c54dad483b31e4c451
dannegm <im@danielgarcia.me>
2021-08-21 09:49:48 -0500

    fix: Correciones varias

--- src/modules/Gavi/hooks/useNavigationDate.js
+++ src/modules/Gavi/hooks/useNavigationDate.js
@@ -1,15 +1,8 @@
-import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import {
-    FORMAT_COMMON,
-    isWeekend,
-    buildDate,
-    getNextWorkingDay,
-    formatDate,
-} from '@helpers/dateHelpers';
+import { FORMAT_COMMON, buildDate, formatDate } from '@helpers/dateHelpers';
 
-const useNavigationDate = (handleNext) => {
+const useNavigationDate = () => {
     const { grade, year, month, day } = useParams();
 
     const today = buildDate(year, month, day);
